81a31abe4f74b583647d2f8bc0c85c6512f0d5ea
Julian Subda <37681273+ActionAnalytics@users.noreply.github.com>
2023-12-30 01:59:24 -0800

    Create setup_helper.sh

---
+++ lib/setup_helper.sh
@@ -0,0 +1,10 @@
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+source "$DIR/shell_helper.sh";
+
+checkDependencies(){
+  if [[ ! code --version ]]
+  then
+      echo -e \\n"Prerequisites not installed.\\nPlease run the prerequisites script per the README.md in the root folder of this repository.\\n"\\n;
+      exit 1;
+  fi
+}
