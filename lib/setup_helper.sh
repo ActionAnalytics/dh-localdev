3138b2ffebd8415519336439297ee6e57a80d61c
Julian Subda <37681273+ActionAnalytics@users.noreply.github.com>
2023-12-30 02:18:03 -0800

    check for podman

--- lib/setup_helper.sh
+++ lib/setup_helper.sh
@@ -2,10 +2,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source "$DIR/shell_helper.sh";
 
 checkDependencies(){
-  if [[ ! code --version ]]
+  if [[ ! "${IDE}" --version ]]
   then
-      echo -e \\n"Prerequisites not installed.\\nPlease run the prerequisites script per the README.md in the root folder of this repository.\\n"\\n;
+      echo -e \\n"Prerequisites IDE "${IDE}" not installed.\\nPlease run the prerequisites script per the README.md in the root folder of this repository.\\n"\\n;
       exit 1;
   fi
-  #check for podman here
+  if [[ ! podman info ]]
+  then
+      echo -e \\n"Prerequisite container service podman not installed.\\nPlease run the prerequisites script per the README.md in the root folder of this repository.\\n"\\n;
+      exit 1;
+  fi
 }
